db47a289fcf3ca535bf9f2f6231f303192349698
Milkitic <Milkitic@126.com>
2021-03-25 23:35:21 +0800

    hito

---
+++ KiaiWaifu.cs
@@ -0,0 +1,105 @@
+using OpenTK;
+using OpenTK.Graphics;
+using StorybrewCommon.Mapset;
+using StorybrewCommon.Scripting;
+using StorybrewCommon.Storyboarding;
+using StorybrewCommon.Storyboarding.Util;
+using StorybrewCommon.Subtitles;
+using StorybrewCommon.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StorybrewScripts
+{
+    public class KiaiWaifu : StoryboardObjectGenerator
+    {
+        [Configurable]
+        public int StartTime = 58620;
+        public override void Generate()
+        {
+            var layer = GetLayer("waifu");
+            var bg = layer.CreateSprite(@"SB\cg\waifu.png", OsbOrigin.CentreRight);
+            bg.Scale(StartTime, 1.5);
+            bg.Rotate((OsbEasing)6, StartTime, 60074, 0.9, 0.2);
+            bg.MoveX((OsbEasing)0, StartTime, 60074, 600, 1100);
+            bg.MoveY((OsbEasing)1, StartTime, 59183, 350, 500);
+            bg.MoveY((OsbEasing)6, 59183, 60074, 500, 520);
+
+            bg.Scale(0, 60074, 60120, 1.5, 1);
+            bg.Rotate(0, 60074, 60120, 0.2, 0.15);
+            bg.MoveX(0, 60074, 60120, 1100, 850);
+            bg.MoveY(0, 60074, 60120, 520, 400);
+
+            bg.Scale((OsbEasing)1, 60120, 60120 + (61620 - 60120) / 2, 1, 0.85);
+            bg.MoveX((OsbEasing)1, 60120, 60120 + (61620 - 60120) / 2, 850, 775);
+            bg.MoveY((OsbEasing)1, 60120, 60120 + (61620 - 60120) / 2, 400, 300);
+            bg.Rotate((OsbEasing)1, 60120, 60120 + (61620 - 60120) / 2, 0.15, 0.025);
+
+            bg.Scale((OsbEasing)2, 60120 + (61620 - 60120) / 2, 61620, 0.85, 0.7);
+            bg.MoveX((OsbEasing)2, 60120 + (61620 - 60120) / 2, 61620, 775, 700);
+            bg.MoveY((OsbEasing)2, 60120 + (61620 - 60120) / 2, 61620, 300, 200);
+            bg.Rotate((OsbEasing)2, 60120 + (61620 - 60120) / 2, 61620, 0.025, -0.1);
+
+            var fixTime = 60120 + (61620 - 60120) / 2;
+            bg.Fade(60120, 1);
+            bg.Fade(0, fixTime - 200, fixTime + 200, 0, 0);
+            bg.Fade(fixTime + 200, 1);
+
+            var bgEasingFix = layer.CreateSprite(@"SB\cg\waifu.png", OsbOrigin.CentreRight);
+            bgEasingFix.Scale(0, fixTime - 200, fixTime + 200, 0.863, 0.841);
+            bgEasingFix.MoveX(0, fixTime - 200, fixTime + 200, 781.5, 771);
+            bgEasingFix.MoveY(0, fixTime - 200, fixTime + 200, 308.5, 292.5);
+            bgEasingFix.Rotate(0, fixTime - 200, fixTime + 200, 0.0355, 0.015);
+
+            var bg2 = layer.CreateSprite(@"SB\cg\waifu.png");
+            bg2.Fade(61620, 1);
+            bg2.Fade(61620 + 600, 0);
+            bg2.Fade(61620 + 1200, 1);
+            bg2.Move((OsbEasing)13, 61620, 61620 + 1000, -500, 300, 160, 300);
+            bg2.Move((OsbEasing)14, 61620 + 900, 61620 + 2000, 160, 300, 600, 300);
+            bg2.Fade(61620 + 1800, 0);
+            bg2.Scale(61620, 0.9);
+
+            var bgEasingFix2 = layer.CreateSprite(@"SB\cg\waifu.png");
+            bgEasingFix2.Fade(61620 + 600, 1);
+            bgEasingFix2.Move(0, 61620 + 600, 62845, 153, 300, 170, 300);
+            bgEasingFix2.Fade(61620 + 1200, 0);
+            bgEasingFix2.Fade(61620 + 1800, 1);
+            bgEasingFix2.Scale(61620 + 1800, 0.9);
+            bgEasingFix2.Scale(61620 + 1800, 0.9);
+            bgEasingFix2.Move(0, 61620 + 1800, 64246, 598, 300, 610, 300);
+
+            var bgRed = layer.CreateSprite(@"SB\cg\waifu_red.png", OsbOrigin.Centre, new Vector2(330, 240));
+            bgRed.MoveY((OsbEasing)10, 64246, 66871, 220, 280);
+            bgRed.MoveX((OsbEasing)10, 64246, 66871, 330, 350);
+            bgRed.Scale((OsbEasing)10, 64246, 66871, 0.5, 0.7);
+
+            var bgRed2 = layer.CreateSprite(@"SB\cg\waifu_red.png", OsbOrigin.Centre, new Vector2(380, 240));
+            bgRed2.Scale((OsbEasing)10, 66871, 67621, 1, 1);
+            bgRed2.MoveY((OsbEasing)10, 66871, 67621, 140, 440);
+            bgRed2.Fade((OsbEasing)10, 67621 - 300, 67621, 1, 0);
+            var bg22 = layer.CreateSprite(@"SB\cg\waifu.png", OsbOrigin.Centre, new Vector2(380, 240));
+            bg22.Scale((OsbEasing)10, 66871, 67621, 1, 1);
+            bg22.Fade((OsbEasing)10, 66871, 67621, 0, 1);
+            bg22.MoveY((OsbEasing)10, 66871, 67621, 140, 440);
+            bg22.MoveY((OsbEasing)9, 67621, 68933, 440, 940);
+
+            var bg22_fixEase = layer.CreateSprite(@"SB\cg\waifu.png", OsbOrigin.Centre, new Vector2(380, 240));
+            bg22.Fade(66871, 1);
+            bg22.Fade(67621, 0);
+            bg22.Fade(67996 + 200, 1);
+
+            bg22_fixEase.Scale((OsbEasing)0, 67621, 67996, 1, 1);
+            bg22_fixEase.Fade(67621, 1);
+            bg22_fixEase.MoveY((OsbEasing)0, 67621, 67996 + 200, 440, 460);
+            // bg.Scale((OsbEasing)0, 60120 + (61620 - 60120) / 2, 60120 + (61620 - 60120), 1, 0.7);
+            // bg.MoveX((OsbEasing)0, 60120 + (61620 - 60120) / 2, 60120 + (61620 - 60120), 850, 700);
+            // bg.MoveY((OsbEasing)0, 60120 + (61620 - 60120) / 2, 60120 + (61620 - 60120), 200, 200);
+            // bg.Rotate((OsbEasing)0, 60120 + (61620 - 60120) / 2, 60120 + (61620 - 60120), -0.3, -0.35);
+
+            // bg.Fade(70009, 1);
+
+        }
+    }
+}
--- Lyric3.cs
+++ Lyric3.cs
@@ -8,6 +8,7 @@ using StorybrewCommon.Subtitles;
 using StorybrewCommon.Util;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -22,39 +23,19 @@ namespace StorybrewScripts
         public override void Generate()
         {
             var layer = GetLayer("lyric");
+            var newStart = StartTime + 45308 - 45120 - One2Eight * 0;
+            var newEnd = newStart + 45683 - 45308;
+            var ouTransStart = new[] { 45683, 45777, 45870, 45964 };
+            var ouTransEnd = 46245/*46058*/- One2Eight;
+
             var bg = layer.CreateSprite(@"SB\components\bg.jpg");
             bg.Fade(StartTime, 0.3);
-            bg.Scale(StartTime - 1000, StartTime + 3000, 0.85, 0.85);
-            // var word1 = layer.CreateSprite(ConvertToFileName("衝", "_st_1L"), OsbOrigin.Centre, new Vector2(170, 210));
-            // word1.Scale(OsbEasing.OutCubic, StartTime, StartTime + One2Eight * 6, 2, 0.5);
-            // word1.Fade(StartTime, StartTime + One2Eight * 2, 1, 1);
-            // word1.Fade(StartTime + One2Eight * 2, 0);
-            // word1.Fade(StartTime + One2Eight * 4, StartTime + One2Eight * 6, 1, 1);
-            // var word1_s = layer.CreateSprite(ConvertToFileName("衝", "_1L"), OsbOrigin.Centre, new Vector2(170, 210));
-            // word1_s.Scale(OsbEasing.OutCubic, StartTime, StartTime + One2Eight * 6, 2, 0.5);
-            // word1_s.Fade(StartTime, 0);
-            // word1_s.Fade(StartTime + One2Eight * 2, StartTime + One2Eight * 4, 1, 1);
+            bg.Fade(OsbEasing.Out, 45683, 45964, 0.3, 0.1);
+            bg.Scale(OsbEasing.Out, StartTime, 47370, 0.9, 0.85);
 
-            int startTime = StartTime;
-            double targetSize = 0.5;
-            double originalSize = targetSize * 1.5;
-            SetAction("衝", "1", "3", layer, new Vector2(80, 110), startTime, targetSize, originalSize);
-
-            startTime = StartTime + One2Eight * 2;
-            targetSize = 0.6;
-            originalSize = targetSize * 1.5;
-            SetAction("衝", "2", "1", layer, new Vector2(310, 240), startTime, targetSize, originalSize);
-
-            startTime = StartTime + One2Eight * 4;
-            targetSize = 0.4;
-            originalSize = targetSize * 1.5;
-            SetAction("衝", "3", "2", layer, new Vector2(570, 280), startTime, targetSize, originalSize);
-
-            var newStart = StartTime + 45308 - 45120;
-            var newEnd = newStart + 45683 - 45308;
             var pngName = Enumerable.Range(1, 3);
             var all = pngName.SelectMany(k => new[] { ConvertToFileName("衝", $"_st_{k}S"), ConvertToFileName("衝", $"_{k}S"), }).ToArray();
-
+            var files = new DirectoryInfo(Path.Combine(MapsetPath, "SB", "output")).GetFiles("*S.png").Select(k => $"SB\\output\\{k.Name}").ToArray();
             var size = 40;
             for (int j = 0; j < 13; j++)
             {
@@ -72,21 +53,95 @@ namespace StorybrewScripts
                     var sprite = layer.CreateSprite(randomName);
                     var trueStart = newStart + Random(200);
 
-                    var color = Random(0.5, 1);
+                    var color = Random(0.5, 0.7);
                     var showRed = Random(2d) >= 1.5;
 
-                    sprite.Move(trueStart, newEnd, -107 + i * size, j * size, -107 + i * size, j * size);
-                    sprite.Fade(OsbEasing.OutExpo, trueStart, newEnd, 0, 1);
-                    sprite.Scale(OsbEasing.OutExpo, trueStart, newEnd, preSize, finalSize);
-                    sprite.Rotate(OsbEasing.OutExpo, trueStart, newEnd, preRotate, finalRotate);
-                    if (!showRed)
-                        sprite.Color(trueStart, color, color, color);
-                    else
+                    var list = new List<OsbSprite>(){
+                        sprite,
+                        layer.CreateSprite(files[Random(files.Length)]),
+                        // layer.CreateSprite(files[Random(files.Length)]),
+                    };
+
+                    var changeTime = ouTransStart[Random(ouTransStart.Length)];
+
+                    for (int index = 0; index < list.Count; index++)
                     {
-                        Log("sb");
-                        sprite.Color(trueStart, 0.7, 0.1f, 0);
+
+                        var fs = index == 0 ? finalSize : finalSize * 0.9;
+                        OsbSprite item = list[index];
+                        item.Move(trueStart, changeTime, -107 + i * size, j * size + Random(0, 20), -107 + i * size, j * size);
+                        item.Scale(OsbEasing.OutExpo, trueStart, newEnd, preSize, fs);
+                        item.Rotate(OsbEasing.OutExpo, trueStart, newEnd, preRotate, finalRotate);
+
+
+                        if (index == 0)
+                        {
+                            item.Fade(trueStart, 0);
+                            item.Fade(changeTime, ouTransEnd, 1, 1);
+
+                            if (!showRed)
+                                item.Color(trueStart, color, color, color);
+                            else
+                                item.Color(trueStart, 0.7, 0.1f, 0);
+
+                            item.Move(ouTransEnd, ouTransEnd + One2Eight + Random(One2Eight * 3), -107 + i * size, j * size, -107 + i * size, j * size - 480);
+                        }
+                        else
+                        {
+                            item.Fade(OsbEasing.OutExpo, trueStart, newEnd, 0, 1);
+                            item.Fade(changeTime, 1);
+                        }
                     }
                 }
+            }
+
+            // var word1 = layer.CreateSprite(ConvertToFileName("衝", "_st_1L"), OsbOrigin.Centre, new Vector2(170, 210));
+            // word1.Scale(OsbEasing.OutCubic, StartTime, StartTime + One2Eight * 6, 2, 0.5);
+            // word1.Fade(StartTime, StartTime + One2Eight * 2, 1, 1);
+            // word1.Fade(StartTime + One2Eight * 2, 0);
+            // word1.Fade(StartTime + One2Eight * 4, StartTime + One2Eight * 6, 1, 1);
+            // var word1_s = layer.CreateSprite(ConvertToFileName("衝", "_1L"), OsbOrigin.Centre, new Vector2(170, 210));
+            // word1_s.Scale(OsbEasing.OutCubic, StartTime, StartTime + One2Eight * 6, 2, 0.5);
+            // word1_s.Fade(StartTime, 0);
+            // word1_s.Fade(StartTime + One2Eight * 2, StartTime + One2Eight * 4, 1, 1);
+
+            DrawLines(layer, StartTime);
+            DrawLines(layer, 45683, 10, 300);
+
+            int startTime = StartTime;
+            double targetSize = 0.5;
+            double originalSize = targetSize * 1.5;
+            SetAction("衝", "1", "3", layer, new Vector2(80, 110), startTime, targetSize, originalSize);
+
+            startTime = StartTime + One2Eight * 2;
+            targetSize = 0.6;
+            originalSize = targetSize * 1.5;
+            SetAction("衝", "2", "1", layer, new Vector2(310, 240), startTime, targetSize, originalSize);
+
+            startTime = StartTime + One2Eight * 4;
+            targetSize = 0.4;
+            originalSize = targetSize * 1.5;
+            SetAction("衝", "3", "2", layer, new Vector2(570, 280), startTime, targetSize, originalSize);
+
+
+        }
+
+        private void DrawLines(StoryboardLayer layer, int startTime, int count = 3, int randomStart = 100)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Random(0);
+                }
+
+                var line = layer.CreateSprite(@"SB\components\line_source.png", OsbOrigin.CentreLeft);
+                var Length = 200 + Random(200);
+                var trueStart = startTime + Random(randomStart);
+                line.ScaleVec(trueStart, trueStart + 100, Length, 0.75, Length, 0.75);
+                line.Move(trueStart, Random(640), Random(380) + 50);
+                line.Rotate(trueStart, Random(Math.PI * 2));
+                line.Fade(trueStart, 0.3);
             }
         }
 
